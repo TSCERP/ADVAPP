ec6d3feeb6633b260449d70f27776f0f11bddf7b
Nguyen Quang Phuoc <134359001+pedscottdev@users.noreply.github.com>
2023-12-14 19:27:30 +0700

    Project Initiation

---
+++ resources/js/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <h1 className="w-full text-blue-500 pt-6 text-2xl font-bold text-center">WELCOME TO AEON DELIGHT VIETNAM</h1>
+            <h2 className="w-full text-blue-500 p-3 text-xl text-center">DỰ ÁN ĐANG ĐƯỢC SET UP</h2>
+        </div>
+    );
+}
+
+export default App;
---
+++ resources/js/src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+          Aeon Delight Vietnam Pro
+        </div>
+    );
+}
+
+export default Home;

b886120a807e7132fcf9f7d4a4f5579e206d2003
ntnfit <ntnfit98@gmail.com>
2024-02-23 09:28:05 +0700

    Refactor MasterDataController and models***

--- app/Http/Controllers/api/MasterDataController.php
+++ app/Http/Controllers/api/MasterDataController.php
@@ -4,22 +4,32 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Permmiter;
-use App\Models\Nego;
-use App\Models\Finals;
+use App\Models\MasterData\Permitter;
+use App\Models\MasterData\NegoApproval;
+use App\Models\MasterData\FinalApproval;
+use Illuminate\Support\Facades\Auth;
 
 class MasterDataController extends Controller
 {
     function getListApprover(Request $request)
     {
-        $permitter =Permmiter::get();
-        $approver = Nego::get();
-        $final = Finals::get();
+        $EmployeeID = Auth::user()->EmployeeID;
+        $permitter = Permitter::where('EmployeeID', $EmployeeID)->get();
+        $approver = NegoApproval::where('EmployeeID', $EmployeeID)->get();
+        $final = FinalApproval::where('EmployeeID', $EmployeeID)->get();
         return response()->json(
             [
-            'permitter' => $permitter, 
-            'approver' => $approver, 
-            'final' => $final
-        ], 200);
+                'permitter' => $permitter,
+                'approver' => $approver,
+                'final' => $final
+            ],
+            200
+        );
+    }
+    function getAllEmployee()
+    {
+    }
+    function getItemCode()
+    {
     }
 }
